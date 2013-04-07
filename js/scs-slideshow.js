8c2a96490370a602d8415160846f393ca63bb216
benvds <ben@pomodoro.nl>
2013-04-07 22:46:15 +0200

    prev and next buttons

--- js/scs-slideshow.js
+++ js/scs-slideshow.js
@@ -7,21 +7,55 @@
         Slideshow = function (element) {
             this.$element = $(element);
             this.$background = $(backgroundSelector);
-            this.slides = $();
+            this.$slides = $();
+
+            this.addSlides();
+            this.addButtons();
         };
 
     Slideshow.prototype = {
         'addSlides': function () {
-            var src, that = this;
+            var src, slide, that = this;
             this.$element.find('img').each(function () {
                 src = $(this).attr('src');
-                that.slides = that.slides.add($('<div class="scs-slide"></div>').css('backgroundImage', 'url(' + src + ')').appendTo(that.$background));
+                slide = $('<div class="scs-slide"></div>').
+                    css('backgroundImage', 'url(' + src + ')').
+                    appendTo(that.$background);
+                that.$slides = that.$slides.add(slide);
             });
         },
 
+        // move buttons inside the $background so aligns to middle
+        'addButtons': function (index) {
+            var $next = $('.scs-next'),
+                $prev = $('.scs-prev');
+
+            $next.on('click', $.proxy(this.showNext, this));
+            $prev.on('click', $.proxy(this.showPrev, this));
+        },
+
+        'isLastSlide': function (index) {
+            return this.$slides.length === index+ 1;
+        },
+
+        'showPrev': function () {
+            var curIndex = this.$background.find('.active').index(),
+                nextIndex = curIndex === 0 ?
+                    this.$slides.length - 1 : curIndex - 1;
+
+            this.setActive(nextIndex);
+        },
+
+        'showNext': function () {
+            var curIndex = this.$background.find('.active').index(),
+                nextIndex = this.isLastSlide(curIndex) ? 0 : curIndex + 1;
+
+            this.setActive(nextIndex);
+        },
+
         'setActive': function (index) {
-            this.slides.removeClass('active');
-            this.slides.eq(index).addClass('active');
+            this.$slides.removeClass('active');
+            this.$slides.eq(index).addClass('active');
         }
     };
 
@@ -35,7 +69,6 @@
                            (data = new Slideshow(this)));
             }
 
-            data.addSlides();
             data.setActive(typeof index == 'number' ? index : 0);
         });
     };
